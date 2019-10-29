bec599f3343c499ea0316f1329345979c342ad7a
remusezequiel <ezequielremus@gmail.com>
2019-10-29 20:32:46 -0300

    modificacion: 29/10/19 20:32

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Haskell-Algebra1
+
+![Logo de Haskell]
+(https://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg)
+
+## Resumen:
+
+Este repositorio se corresponde con lo visto en el taller correspondiente a la materia **Algebra-1** para *Matematicos* y *Ciencias de la computación*de la UBA. En este se dan las bases de la programacion en (*[Haskell] [1]*), el cual es un lenguage de programación funcional basado el lambda funciones. 
+
+## Instalacion:
+
+GHCI (The Glasgow Haskell Compiller Interactive enviroment)
+-----------------------------------------------------------
+1. **Ubuntu** : *sudo apt-get install ghc*
+
+2. **Mac** : *brew install haskell-platform*
+
+3. **Windows** : (*[https://www.haskell.org/platform/windows.html] [2]*)
+
+##¿Como usar lo que esta en el repo?
+
+Lo que tenes que hacer para poder ejecutar las funciones de cada archivo es: 
+
+* Primero: 
+  *  Te paras sobre la carpeta donde estan los archivos y abris una terminal
+* Segundo:
+  *  En la terminal escribis *"ghci"*. Estao te abre el prelude de Haskell.
+* Tercero:
+  *  Llamas al archivo con el comando *:l*. Ejemplo: *:l Clase1.hs*
+* Cuarto: 
+  *  Ejecutas las funciónes
+
+> **Cuidado!**. Tenes que tener en cuenta que para Haskell un espacio en blanco es un operador. Luego, para llamar por ejemplo a la función *suma*, la cual recibe dos parametros, en la terminal deberas llamarla *suma 3 4*, luego este de hara la suma y te tira como resultado 7 debajo, pero es incorrecto si escribes *suma 34*. Lo mismo si haces *3+4* pegado en el prelude, debes escribirlo de la siguiente forma *3 + 4*. 
+
+
+
+[1]:https://www.haskell.org
+[2]:https://www.haskell.org/platform/windows.html
--- haskell/Clase7.hs
+++ haskell/Clase7.hs
@@ -2,7 +2,115 @@ module Clase7
 where
 
 
-factorial :: Integer -> Integer
-factorial 0 = 1
-factorial n = n
+{-    LISTAS 
+
+   Algunas operaciones: 
+
+head :: [a] -> a
+tail :: [a] -> [a]
+(:) :: a -> [a] -> [a]
+(++) :: [a] -> [a] -> [a]
+length :: [a] -> Integer
+
+-}
+
+
+{- Funcion sumatoria: 
+
+  Es un sumador de elementos dentro de una lista
+  toma la cabeza y se lo suma de forma recursiva a la
+  cabeza de la cola llamandose a si misma
+-}
+sumatoria :: [Integer] -> Integer
+sumatoria lis | length lis == 0 = 0
+              | otherwise = (head lis) + sumatoria (tail lis)
+
+{- Funcion pertenece: 
+  
+  Se fija si un elemento "K" pertenece o no a la lista 
+  siendo tanto el elemento como la lista un Integer
+  Esto lo hace de forma recursiva, primero evalua si en 
+  la cabeza de la lista esta el elemento k, sino se llama
+  a si misma pasandose la cola de la lista y el elemento k
+  para volver a evaluar sobre la cabeza de la cola. 
+
+-}
+pertenece :: Integer -> [Integer] -> Bool
+pertenece k lis | length lis == 0 = False
+                | k == head lis = True
+                | otherwise = pertenece k (tail lis)
+------------------------------------------------------------------
+--  Definir la función listar :: a-> a -> a -> [a] que toma tres elementos y los
+--los convierte en una lista
+
+listar :: a -> a -> a -> [a]
+listar p q k = p:q:k:[] 
+
+{-ejemplo de funcionamiento: listar 1 2 3, te devuelve [1,2,3]-} 
+------------------------------------------------------------------
+
+------------------------------------------------------------------
+{-
+  Definir la funcion newPertenece :: Ep a => a -> [a] -> Bool que indica si el elemento
+que se le pasa está o no en la lista.
+-}
+newPertenece :: (Eq a) => a -> [a] -> Bool
+newPertenece n lis | length lis == 0 = False
+                   | (head lis) == n = True
+                   | otherwise = newPertenece n (tail lis)
+
+
+------------------------------------------------------------------
+productoria:: [Integer] -> Integer
+productoria ls | length ls == 0 = 1
+               | otherwise = (head ls)*productoria (tail ls)
+------------------------------------------------------------------
+
+------------------------------------------------------------------
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN n ls | length ls == 0 = []
+            | otherwise = ((head ls)+n) : (sumarN n (tail ls))
+------------------------------------------------------------------
+
+
+
+--¿COMO ESCRIBIR LA FUNCION SUMATORIA USANDO PATTERN MATCHING?
+
+sumPattern :: [Integer] -> Integer
+sumPattern [] = 0
+sumPattern (x:xs) = sumPattern xs + x
+
+{-
+    Comentario:
+
+      Las listas también admiten el aptron _ 
+      que se corresponde con cualquier valor
+      pero no liga ninguna variable
+-}
+
+
+longitud :: [a] -> 
+longitud [] = 0
+longitud (_:xs) = 1 + longitud xs
+
+--Redefinicion de newPertenece pero en pattern matching
+pertein :: (Eq a) => a -> [a] -> Bool
+pertein [] = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
